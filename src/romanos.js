e9a309562487bbdbbecc9332797850500b28ffb3
Carla_Diana <carladianavgonzales@gmail.com>
2025-03-04 19:12:11 -0400

    feat: Convertir 1 a 'I'

---
+++ src/romanos.js
@@ -0,0 +1,7 @@
+function convertirARomanos(num) {
+    if(num == 1){
+        return "I"
+    }
+}
+
+export default convertirARomanos;
---
+++ src/romanos.test.js
@@ -0,0 +1,8 @@
+import convertirARomanos from"./romanos.js"
+
+describe("Conversión de números a romanos", () => {
+    it("Debería convertir 1 a 'I'", () => {
+        expect(convertirARomanos(1)).toEqual("I");
+    });
+    
+});
